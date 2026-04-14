4af59dc4532c14b12ef0425222535d0d7806d3d2
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2026-04-14 08:10:51 +0200

    feat(to not be included): Add InmemoryProvider implementation

--- api/src/main/java/info/touret/musicstore/infrastructure/featureflag/adapter/DiscountAdapter.java
+++ api/src/main/java/info/touret/musicstore/infrastructure/featureflag/adapter/DiscountAdapter.java
@@ -1,30 +1,42 @@
 package info.touret.musicstore.infrastructure.featureflag.adapter;
 
+import dev.openfeature.sdk.OpenFeatureAPI;
 import info.touret.musicstore.domain.model.Instrument;
 import info.touret.musicstore.domain.model.Result;
 import info.touret.musicstore.domain.model.User;
 import info.touret.musicstore.domain.port.DiscountPort;
 import jakarta.enterprise.context.ApplicationScoped;
-
-// TODO: Chapter 3 - Uncomment to use OpenFeature
-// import dev.openfeature.sdk.Client;
-// import dev.openfeature.sdk.MutableContext;
-// import dev.openfeature.sdk.OpenFeatureAPI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class DiscountAdapter implements DiscountPort {
+
+    private final OpenFeatureAPI openFeatureAPI;
+    private static final Logger LOGGER = LoggerFactory.getLogger(DiscountAdapter.class);
+
+    public DiscountAdapter(OpenFeatureAPI openFeatureAPI) {
+        this.openFeatureAPI = openFeatureAPI;
+    }
 
     @Override
     public Result<Instrument> applyDiscount(Instrument instrument, User user) {
         // TODO: Chapter 3 - Implement this with OpenFeature
         // For now, let's keep it simple (manually toggle for testing if needed)
-        boolean manualDiscount = false; // Toggle to true to test UI
-        if (manualDiscount) {
+//        boolean manualDiscount = false; // Toggle to true to test UI
+//        if (manualDiscount) {
+//            double originalPrice = instrument.price();
+//            double discountedPrice = originalPrice * 0.9; // 10% discount
+//            return Result.success(instrument.withDiscount(discountedPrice, originalPrice));
+//        }
+        var evaluationDetails = this.openFeatureAPI.getClient().getBooleanDetails("wrong-discount-enabled", false);
+        LOGGER.info(evaluationDetails.toString());
+        boolean isDiscountEnabled = evaluationDetails.getValue();
+        if (isDiscountEnabled) {
             double originalPrice = instrument.price();
             double discountedPrice = originalPrice * 0.9; // 10% discount
             return Result.success(instrument.withDiscount(discountedPrice, originalPrice));
         }
-
         return Result.success(instrument);
     }
 }
